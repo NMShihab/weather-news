79d4dc22589e03b396fdeb6ae240f5d6f5f9d52f
NMShihab <nmshihabislam@gmail.com>
2020-08-11 10:43:02 +0600

    make components

--- src/components/weathercard/components.js
+++ src/components/weathercard/components.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import styled  from '@emotion/styled';
-import Location from './Location';
-
-
+import React from "react";
+import styled from "@emotion/styled";
+import Location from "./Location";
+import Icon from "./Icon";
+import Condition from "./Condition";
 
 const Weathercard = (props) => {
-    const Card = styled.div`
-        margin: 0 auto;
-        background: linear-gradient(to bottom, red,pink);
-        width: 200px;
-        height: 240px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        border-radius: 20px;  
-    `
-    return ( 
-        <Card >
+  const Card = styled.div`
+    margin: 0 auto;
+    background: linear-gradient(to bottom, red, pink);
+    width: 200px;
+    height: 240px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 20px;
+  `;
+  return (
+    <Card>
+      <Location />
+      <Icon />
+      <Condition />
+    </Card>
+  );
+};
 
-            <Location/>
-            <img className="icon" src="./img/cloud.jpg" alt=""/>
-            <h1 className="temp">20 °C</h1>
-            <h3 className="condition">clouds</h3>
-        </Card>
-     );
-}
- 
 export default Weathercard;
