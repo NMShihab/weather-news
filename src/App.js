0bde8d42bbf4a814a3ff864eaf2b74ceff20ecd4
NMShihab <nmshihabislam@gmail.com>
2020-08-18 22:20:38 +0600

    setting weather api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import "./App.css";
 import Weathercard from "./components/weathercard/components";
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=Dhaka,ban&units=celsius&APPID=a6338b6dcaaf984c48833bda656ad993"
+    );
+    const resJson = await apiRes.json();
+    return resJson;
+  };
+  data().then((res) => console.log(res));
   return (
     <div className="App">
       <Weathercard temp={30} condition="Clear" city="Dhaka" country="BAN" />
