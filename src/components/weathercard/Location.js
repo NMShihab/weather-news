c178069dd193c15475b12ba9754c2addf5f627e5
NMShihab <nmshihabislam@gmail.com>
2020-08-26 10:46:56 +0600

    style search box

--- src/components/weathercard/Location.js
+++ src/components/weathercard/Location.js
@@ -1,27 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "@emotion/styled";
 
 const Location = ({ city, country, getWeather }) => {
   const [query, setQuery] = useState("");
   const [inputMode, setInputMode] = useState(false);
+  const inputRef = useRef("");
+
+  useEffect(() => {
+    if (inputMode) {
+      inputRef.current.focus();
+    }
+  }, [inputMode]);
 
   if (inputMode) {
     return (
       <Container>
-        <form
+        <FormElement
           onSubmit={(e) => {
             e.preventDefault();
             getWeather(query);
           }}
         >
-          <input
+          <InputField
+            ref={inputRef}
             required
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
-          <button type="submit">Search</button>
-          <button onClick={() => setInputMode(false)}>Cancel</button>
-        </form>
+          <SearchButton type="submit">Search</SearchButton>
+          <CancelButton onClick={() => setInputMode(false)}>X</CancelButton>
+        </FormElement>
       </Container>
     );
   }
@@ -38,6 +46,46 @@ export default Location;
 
 const Container = styled.div`
   text-align: center;
+`;
+
+const FormElement = styled.form`
+  display: flex;
+  position: relative;
+  background: #46618b;
+  border-radius: 5px;
+`;
+const InputField = styled.input`
+  padding: 4px;
+  width: 80px;
+  background: transparent;
+  color: white;
+  border: none;
+  &:focus {
+    outline: 0;
+  }
+`;
+const SearchButton = styled.button`
+  padding: 4px;
+  background: #394e70;
+  border: none;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  color: white;
+  cursor: pointer;
+`;
+const CancelButton = styled.span`
+  position: absolute;
+  background: #557fc2;
+  top: -10px;
+  right: -10px;
+  cursor: pointer;
+  width: 17px;
+  height: 17px;
+  border-radius: 50%;
+  font-size: 0.8rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const City = styled.h1`
   font-family: "Merriweather", serif;
